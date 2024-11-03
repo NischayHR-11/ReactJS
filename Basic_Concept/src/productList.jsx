98ed28db4ad046588a3e46981f2b34516ca4717c
NischayHR-11 <nischayhr11@gmail.com>
2024-11-04 01:23:03 +0530

    features added To Products

--- Basic_Concept/src/productList.jsx
+++ Basic_Concept/src/productList.jsx
@@ -3,11 +3,12 @@ import "./productList.css"
 
 function ProductList({name1,name2,name3}){
 
+    let options=["hi-tech","Durable","Electronical"];
     return (
         <div className="List">
-        <Product name={name1} price={20000}/>
-        <Product name={name2} price={30000}/>
-        <Product name={name3} price={40000}/>
+        <Product name={name1} price={20000} features={options}/>
+        <Product name={name2} price={30000} features={options}/>
+        <Product name={name3} price={40000} features={options}/>
         </div>
     )
 }
