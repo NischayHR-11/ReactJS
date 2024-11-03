a373164f9b9e789ca630ab3251d4e6ffc6ae4af6
NischayHR-11 <nischayhr11@gmail.com>
2024-11-04 01:21:42 +0530

    Adde Dynamic Styling And Conditional statements

--- Basic_Concept/src/product.jsx
+++ Basic_Concept/src/product.jsx
@@ -2,13 +2,20 @@ import Title from "./Title";
 import { Description } from './description.jsx';
 import "./product.css"
  
-function Product({name,price=1}){
+function Product({name,price=1,features}){
+    
+    // Altering The Array Elements With Li.
+    const list= features.map((feature)=><li>{feature}</li>);
+    // Dynamic Styling with conditions.
+    const styles={backgroundColor: price>=30000?"green":null};
 
     return(
-
-        <div className="product">
+        <div className="product" style={styles}>
             <Title name={name}/>
             <Description price={price}/>
+            <p>{list}</p>
+            {/* conditional Statements */}
+            {price >= 30000?<p>" 5% Discount.."</p>:null}     
         </div>
     )
 }
